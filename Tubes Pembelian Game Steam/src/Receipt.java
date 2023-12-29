20d6320c8dddad78d557978bf25dcb04e21a04c5
SotoAyamMangDul <aryakaml24@gmail.com>
2023-12-30 01:04:05 +0700

    TUBES OOP 2023

---
+++ Tubes Pembelian Game Steam/src/Receipt.java
@@ -0,0 +1,34 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+class Receipt {
+    private String fileName;
+
+    public Receipt(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void generateReceipt(ArrayList<SteamGame> selectedGames, double totalharga) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
+            writer.println("==============================");
+            writer.println(" Receipt pembelian Steam Game ");
+            writer.println("==============================");
+
+            for (SteamGame game : selectedGames) {
+                writer.println("Game: " + game.nama);
+                writer.println("harga: RP" + game.harga);
+                writer.println();
+            }
+            writer.println("------------------------------");
+            writer.println("Total Amount: RP" + totalharga);
+            writer.println("==============================");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Receipt pembayaran dicetak. Cek file: " + fileName);
+    }
+}
